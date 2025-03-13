4014d6062b2a1d3ecf874d00a5a6e9b88620d620
krishnakanth <krishnakanthpathi@gmail.com>
2025-03-13 18:19:31 +0530

    prevous changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@
  * doubt
  * teacher
  * 
- */
+ **/
 
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 // import db
